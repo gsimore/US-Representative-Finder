ad9cc0c59ee30b1bc1135a8f0cd1116d7e35f8d7
gsimore <gsimardmoore@gmail.com>
2021-10-17 22:46:47 -0700

    Get representative info and display to user
    * Adds utility functions that load the data files into pandas, does some cleaning and filtering on the data, and then looks up representatives by zip code. 
    * Adds first, rough and slow, working version of looking up a representative and displaying it back to the user. Currently just displays a dictionary in the template.

--- find_my_rep/app.py
+++ find_my_rep/app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
-
+from utilities.representative_lookup import lookup_representatives
 app = Flask(__name__)
 
 
 @app.route('/', methods=['post', 'get'])
 def find_representative():
+    reps = []
     message = ''
     if request.method == 'POST':
         zipcode = request.form.get('zipcode')
         if zipcode:
-            # TODO: Look up representative by zipcode and return summary
-            pass
+            reps = lookup_representatives(zipcode)
         else:
             message = "Please enter a zipcode"
 
-    return render_template('find_my_rep.html', message=message)
+    return render_template('find_my_rep.html', message=message, reps=reps)
---
+++ find_my_rep/utilities/representative_lookup.py
@@ -0,0 +1,90 @@
+import datetime
+import json
+import os
+
+import pandas
+from flask import Flask
+
+app = Flask(__name__)
+
+ACTIVE_DATE = datetime.date(2019, 1, 1)
+DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(app.root_path)), 'data')
+LEGISLATORS_FILE_PATH = os.path.join(DATA_PATH, 'legislators.json')
+ZIPCODE_DISTRICTS_FILE_PATH = os.path.join(DATA_PATH, 'zipcode-districts.json')
+
+
+class ZipcodeNotFound(Exception):
+    # Raise this when a zipcode is not indexed in the dataset.
+    pass
+
+class DistrictNotFound(Exception):
+    # Raise this when a state and district is not indexed in the legislators dataset.
+    pass
+
+
+def get_states_and_districts(zipcode):
+    """
+    Looks up the state and district for the given zipcode inside the
+    zipcode-districts.json data file.
+    :param zipcode: int, a US Zipcode
+    :return: list of tuples of states and districts
+    """
+    df = pandas.read_json(ZIPCODE_DISTRICTS_FILE_PATH, dtype={'zcta': 'str'})
+    df = df.set_index('zcta')
+    try:
+        data = df.loc[zipcode]
+    except KeyError:
+        raise ZipcodeNotFound
+
+    state = data['state_abbr']
+    district = data['cd']
+    # Sometimes a zipcode is associated to more than one district.
+    if type(data) == pandas.Series:
+        return [(state, district)]
+    else:
+        return list(zip(state.tolist(), district.tolist()))
+
+def get_representative(state, district):
+    """
+    Looks up the US Representative for the given state and district.
+    :param state: str,
+    :param district: int
+    :return:
+    """
+    with open(LEGISLATORS_FILE_PATH) as f:
+        data = json.loads(f.read())
+
+    df = pandas.json_normalize(data, "terms", ['id', ['name', 'official_full'], 'bio'])
+
+    df = df.set_index(['state', 'district'])
+
+    # Filter to representatives only.
+    df = df[(df['type'] == 'rep')]
+
+    # Convert 'start' and 'end' columns to dates
+    df[['start', 'end']] = df[['start', 'end']].apply(lambda x: pandas.to_datetime(x).dt.date)
+    # Filter to current terms.
+    df = df[(df['start'] <= ACTIVE_DATE) & (df['end'] >= ACTIVE_DATE)]
+
+    try:
+        rep_data = df.loc[state, district]
+    except KeyError:
+        raise DistrictNotFound
+
+    return rep_data
+
+
+def lookup_representatives(zipcode):
+    states_districts = get_states_and_districts(zipcode)
+    reps = []
+    for state, district in states_districts:
+        data = get_representative(state, district)
+        reps.append({
+            'state': state,
+            'district': district,
+            'name': data['name.official_full'],
+            'phone': data['phone'],
+            'address': data['address'],
+            'url': data['url'],
+        })
+    return reps
